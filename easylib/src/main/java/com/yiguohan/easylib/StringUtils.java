25f7b428358b07c276471d7395d17e5642230623
yiguohan <yiguohan@gmail.com>
2018-01-28 17:39:05 +0800

    Jan 28th 2018 update

--- easylib/src/main/java/com/yiguohan/easylib/StringUtils.java
+++ easylib/src/main/java/com/yiguohan/easylib/StringUtils.java
@@ -10,6 +10,27 @@ package com.yiguohan.easylib;
 public class StringUtils {
 
     private StringUtils() {
+        throw new UnsupportedOperationException("不能初始化该类为对象！");
+    }
+
+    /**
+     * 判断字符串是否为 null 或长度为 0
+     *
+     * @param s 待校验字符串
+     * @return {@code true}: 空<br> {@code false}: 不为空
+     */
+    public static boolean isEmpty(final CharSequence s) {
+        return s == null || s.length() == 0;
+    }
+
+    /**
+     * 判断字符串是否为 null 或全为空格
+     *
+     * @param s 待校验字符串
+     * @return {@code true}: null 或全空格<br> {@code false}: 不为 null 且不全空格
+     */
+    public static boolean isTrimEmpty(final String s) {
+        return (s == null || s.trim().length() == 0);
     }
 
     /**
@@ -29,5 +50,106 @@ public class StringUtils {
             }
         }
         return true;
+    }
+
+    /**
+     * 判断两字符串是否相等
+     *
+     * @param a 待校验字符串 a
+     * @param b 待校验字符串 b
+     * @return {@code true}: 相等<br>{@code false}: 不相等
+     */
+    public static boolean equals(final CharSequence a, final CharSequence b) {
+        if (a == b) {
+            return true;
+        }
+        int length;
+        if (a != null && b != null && (length = a.length()) == b.length()) {
+            if (a instanceof String && b instanceof String) {
+                return a.equals(b);
+            } else {
+                for (int i = 0; i < length; i++) {
+                    if (a.charAt(i) != b.charAt(i)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断两字符串忽略大小写是否相等
+     *
+     * @param a 待校验字符串 a
+     * @param b 待校验字符串 b
+     * @return {@code true}: 相等<br>{@code false}: 不相等
+     */
+    public static boolean equalsIgnoreCase(final String a, final String b) {
+        return a == null ? b == null : a.equalsIgnoreCase(b);
+    }
+
+
+    /**
+     * null 转为长度为 0 的字符串
+     *
+     * @param s 待转字符串
+     * @return s 为 null 转为长度为 0 字符串，否则不改变
+     */
+    public static String null2Length0(final String s) {
+        return s == null ? "" : s;
+    }
+
+    /**
+     * 返回字符串长度
+     *
+     * @param s 字符串
+     * @return null 返回0，其他返回自身长度
+     */
+    public static int length(final CharSequence s) {
+        return s == null ? 0 : s.length();
+    }
+
+    /**
+     * 首字母大写
+     *
+     * @param s
+     * @return
+     */
+    public static String upperFirstLetter(final String s) {
+        if (isEmpty(s) || !Character.isLowerCase(s.charAt(0))) {
+            return s;
+        }
+        return String.valueOf((char) (s.charAt(0) - 32)) + s.substring(1);
+    }
+
+    /**
+     * 首字母小写
+     *
+     * @param s
+     * @return
+     */
+    public static String lowerFirstLetter(final String s) {
+        if (isEmpty(s) || !Character.isUpperCase(s.charAt(0))) {
+            return s;
+        }
+        return String.valueOf((char) (s.charAt(0) + 32)) + s.substring(1);
+    }
+
+    public static String reverse(final String s) {
+        int len = length(s);
+        if (len <= 1) {
+            return s;
+        }
+        int mid = len >> 1;//除以2
+        char[] chars = s.toCharArray();
+        char c;
+        for (int i = 0; i < mid; ++i) {
+            c = chars[i];
+            chars[i] = chars[len - i - 1];
+            chars[len - i - 1] = c;
+        }
+        return new String(chars);
     }
 }
